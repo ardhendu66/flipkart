b21133d4576a4c477cc607ce28b39289dc7c4c5d
ardhenduroy67 <95483892+ardhenduroy67@users.noreply.github.com>
2024-03-07 02:40:02 +0530

    commit again

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './store.js'
 import { ApiProvider } from '@reduxjs/toolkit/query/react'
-import { apiSlice } from './features/Product/apiSlice.js'
+import { apiSlice } from './features/Product/api/apiSlice.js'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
-    {/* <Provider store={store}>
-      <App />
-    </Provider> */}
     <ApiProvider api={apiSlice}>
       <App/>
     </ApiProvider>
--- client1/src/features/Product/ProductList/ProductList.jsx
+++ client1/src/features/Product/ProductList/ProductList.jsx
@@ -1,82 +1,102 @@
-import { useState, Fragment } from "react"
+import { useState, useEffect, Fragment } from "react"
 import { NavLink } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
 import { Dialog, Disclosure, Transition, Menu } from "@headlessui/react"
 import { XMarkIcon } from "@heroicons/react/24/outline"
 import { MinusIcon, PlusIcon, FunnelIcon, StarIcon, ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon, Squares2X2Icon } from "@heroicons/react/20/solid"
+import { 
+    selectAllProducts, 
+    fetchProductsAsync, 
+    getProductsStatus, 
+    fetchProductsByFilterAsync 
+} from "../productListSlice"
 import { filters, classNames, sortOptions } from "./productData"
-import { useGetProductsQuery } from "../api/apiSlice"
+import { useGetProductsQuery } from "../apiSlice"
 
 export default () => {
     const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
     const [filter, setFilter] = useState({})
+    // const dispatch = useDispatch()
+    // const [products] = useSelector(selectAllProducts)
+    // const status = useSelector(getProductsStatus)
     const {data: products, isLoading, isError, isSuccess, error } = useGetProductsQuery()
+
+    // useEffect(() => {
+    //     if (status === 'idle') {
+    //         dispatch(fetchProductsAsync())
+    //     }
+    // }, [dispatch, status])
 
     const handleFilter = (filterName, option) => {
         // event.preventDefault()
         // let newFilter = {...filter, [filterName.id]: option.value}
         console.log(event.target.checked)
         setFilter(prev => ({...prev, ...{[filterName.id]: option.value}}))
+        // dispatch(fetchProductsByFilterAsync(filter))
     }
 
     const handleSort = (option) => {
         let newFilter = {...filter, _sort: option.sort,}
         setFilter(newFilter)
+        // dispatch(fetchProductsByFilterAsync(filter))
     }
 
+    if(isSuccess) {
+        return (
+            <div className="bg-white mt-16">
+                <div>
+                    <MobileFilterPanel
+                        mobileFiltersOpen={mobileFiltersOpen}
+                        setMobileFiltersOpen={setMobileFiltersOpen}
+                        handleFilter={handleFilter}
+                    />
 
-    if(isError) {
+                    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                        <div 
+                            className="flex items-baseline justify-between border-b border-gray-200 pb-6"
+                        >
+                            <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+                                All Products
+                            </h1>
+                            <div className="flex items-center">
+                                <SortFunctionality handleSort={handleSort} />
+                                <ViewGrid />
+                                <FunnelFilteringButton setMobileFiltersOpen={setMobileFiltersOpen} />
+                            </div>
+                        </div>
+
+                        <section aria-labelledby="products-heading" className="pb-24 pt-6">
+                            <h2 id="products-heading" className="sr-only">
+                                Products
+                            </h2>
+
+                            <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+                                {/* Filters */}
+                                <form className="hidden lg:block">
+                                    <h3 className="sr-only">Categories</h3>
+                                    <DesktopFilters handleFilter={handleFilter} />
+                                </form>
+
+                                {/* Product grid */}
+                                <ProductGrid
+                                    // status={status}
+                                    status={''}
+                                    products={products}
+                                />
+                            </div>
+                        </section>
+                    </main>
+                </div>
+                <Pagination />
+            </div>
+        )
+    }
+    else if(isError) {
         return <div>{error}</div>
     }
     else if(isLoading) {
         return <div>loading...</div>
     }
-
-    return (
-        <div className="bg-white mt-16">
-            <div>
-                <MobileFilterPanel
-                    mobileFiltersOpen={mobileFiltersOpen}
-                    setMobileFiltersOpen={setMobileFiltersOpen}
-                    handleFilter={handleFilter}
-                />
-
-                <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    <div 
-                        className="flex items-baseline justify-between border-b border-gray-200 pb-6"
-                    >
-                        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-                            All Products
-                        </h1>
-                        <div className="flex items-center">
-                            <SortFunctionality handleSort={handleSort} />
-                            <ViewGrid />
-                            <FunnelFilteringButton setMobileFiltersOpen={setMobileFiltersOpen} />
-                        </div>
-                    </div>
-
-                    <section aria-labelledby="products-heading" className="pb-24 pt-6">
-                        <h2 id="products-heading" className="sr-only">
-                            Products
-                        </h2>
-
-                        <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
-                            {/* Filters */}
-                            <form className="hidden lg:block">
-                                <h3 className="sr-only">Categories</h3>
-                                <DesktopFilters handleFilter={handleFilter} />
-                            </form>
-
-                            {/* Product grid */}
-                            <ProductGrid
-                                products={products}
-                            />
-                        </div>
-                    </section>
-                </main>
-            </div>
-            <Pagination />
-        </div>
-    )
 }
 
 const MobileFilterPanel = ({mobileFiltersOpen, setMobileFiltersOpen, handleFilter}) => {
@@ -264,7 +284,9 @@ const DesktopFilters = ({handleFilter}) => {
     )
 }
 
-const ProductGrid = ({products}) => {
+{/* status === 'loading' ? <h3>loading...</h3> : */}
+{/* status === 'failed' ? <h3>Error...</h3> : */}
+const ProductGrid = ({status, products}) => {
     return (
         <>
             {
